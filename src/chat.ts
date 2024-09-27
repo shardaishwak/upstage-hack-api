61880887987ac2ae7b85dd99d4f61851e6dead54
Ishwak Sharda <ishwak.sharda@gmail.com>
2024-09-27 10:05:13 -0700

    feat: various

--- src/chat.ts
+++ src/chat.ts
@@ -2,6 +2,7 @@ import { ChatCompletionMessageParam } from 'openai/resources';
 import { upstage } from './config/upstage';
 import { availableFunctions, tools } from './tools';
 import { Server, Socket } from 'socket.io';
+import { google_tool_functions, google_tools } from './google_tools';
 
 const generateTitleFromQuery = async (q: string) => {
 	// Generate a nice title to show based on what the user searched for
@@ -17,6 +18,71 @@ const generateTitleFromQuery = async (q: string) => {
 	console.log(JSON.stringify(response, null, 2));
 	const responseMessage = response.choices[0].message;
 	return responseMessage.content;
+};
+
+export const handleChatV2 = async (q: string, io?: Server) => {
+	const data: { [key: string]: any } = {};
+	const preferences: any[] = [];
+
+	const messages: ChatCompletionMessageParam[] = [
+		{
+			role: 'system',
+			content: `For flights, show only 5 results. When showing activities, show at least 15. For activity, field "city" can be also something complex, not just the city. For flights, use type=1 for round flights and type=2 for one-way. This is understood based on whether the user added a return flight or not.
+               For events, add the location (place) inside the query as well `,
+		},
+	];
+
+	messages.push({
+		role: 'user',
+		content:
+			(q as string) +
+			'. Preferences: ' +
+			preferences.join(', ') +
+			'. If user added return flight, type=1, otherwise type=2.',
+	});
+	const response = await upstage.chat.completions.create({
+		model: 'solar-1-mini-chat',
+		messages,
+		tools: google_tools,
+	});
+	const responseMessage = response.choices[0].message;
+
+	const toolCalls = responseMessage.tool_calls;
+	if (toolCalls) {
+		console.log('[toolCalls]', toolCalls);
+		// Step 3: call the function
+		// Note: the JSON response may not always be valid; be sure to handle errors
+		messages.push(responseMessage); // extend conversation with assistant's reply
+		for (const toolCall of toolCalls) {
+			const functionName = toolCall.function.name as keyof typeof google_tool_functions;
+			io?.emit('chat:loading:tool_call', { functionName, loading: true });
+
+			const functionToCall = google_tool_functions[functionName];
+			if (!functionToCall) {
+				throw new Error(`Function ${functionName} does not exist`);
+			}
+			const functionArgs = JSON.parse(toolCall.function.arguments);
+			console.log('[functionArgs]', functionArgs);
+
+			let functionResponse = await functionToCall(functionArgs);
+
+			io?.emit('chat:loading:tool_call', { functionName, loading: false });
+
+			data[functionName] = functionResponse;
+
+			// messages.push({
+			// 	tool_call_id: toolCall.id,
+			// 	role: 'tool',
+
+			// 	content: JSON.stringify(functionResponse),
+			// });
+		}
+		console.log('[data]', data);
+	}
+
+	const title = await generateTitleFromQuery(q);
+	data['title'] = title;
+	return data;
 };
 
 export const handleChat = async (q: string, io?: Server) => {
@@ -81,4 +147,19 @@ export const handleChat = async (q: string, io?: Server) => {
 	const title = await generateTitleFromQuery(q);
 	data['title'] = title;
 	return data;
+};
+
+export const generateItinerary = async (q: string) => {
+	const response = await upstage.chat.completions.create({
+		model: 'solar-pro',
+		messages: [
+			{
+				role: 'user',
+				content: `"${q}". Include the ID`,
+			},
+		],
+	});
+
+	const responseMessage = response.choices[0].message;
+	return responseMessage.content;
 };
---
+++ src/config/serp/getGoogleFlights.ts
@@ -0,0 +1,129 @@
+import axios from 'axios';
+
+export interface GoogleFlightData {
+	best_flights: {
+		id: string;
+		flights: {
+			departure_airport: { name: string; id: string; time: string };
+			arrival_airport: { name: string; id: string; time: string };
+			duration: number;
+			airplane: string;
+			airline: string;
+			airline_logo: string;
+			travel_class: string;
+			flight_number: string;
+			extensions: string[];
+			ticket_also_sold_by: string[];
+			legroom: string;
+			overnight: boolean;
+			often_delayed_by_over_30_min: boolean;
+		}[];
+		layovers: {
+			duration: number;
+			name: string;
+			id: string;
+			overnight: boolean;
+		}[];
+		total_duration: number;
+		carbon_emissions: {
+			this_flight: number;
+			typical_for_this_route: number;
+			difference_percent: number;
+		};
+		price: number;
+		type: string;
+		airline_logo: string;
+		extensions: string[];
+		departure_token: string;
+		booking_token: string;
+	}[];
+	other_flights?: GoogleFlightData['best_flights'];
+	price_insights?: {
+		lowest_price: number;
+		price_level: string;
+		typical_price_range: [number, number];
+		price_history: [number, number][];
+	};
+	airports: {
+		departure: {
+			airport: { name: string; id: string };
+			city: string;
+			country: string;
+			country_code: string;
+			image: string;
+			thumbnail: string;
+		}[];
+		arrival: {
+			airport: { name: string; id: string };
+			city: string;
+			country: string;
+			country_code: string;
+			image: string;
+			thumbnail: string;
+		}[];
+	}[];
+}
+
+export const departureFlightCache = new Map<string, any>();
+
+export const generateGoogleFlightID = (data: GoogleFlightData['best_flights'][number]) => {
+	const flightIds = data.flights.map((flight) => flight.airline + flight.flight_number);
+
+	return flightIds.join('_');
+};
+
+export const getGoogleFlights = async (params: any): Promise<GoogleFlightData | null> => {
+	try {
+		const response: any = await axios.get(
+			'https://serpapi.com/search.json?engine=google_flights',
+			{
+				params: {
+					...params,
+					api_key: process.env.SERPAPI_ACCESS_TOKEN,
+				},
+			}
+		);
+
+		// insert the ID into the data
+		response.data?.best_flights?.forEach((flight: any) => {
+			flight.id = generateGoogleFlightID(flight);
+		});
+
+		response.data.other_flights?.forEach((flight: any) => {
+			flight.id = generateGoogleFlightID(flight);
+		});
+
+		// cache all best flights and other flights
+		response.data.best_flights?.forEach((flight: any) => {
+			departureFlightCache.set(flight.id, flight);
+		});
+
+		response.data.other_flights?.forEach((flight: any) => {
+			departureFlightCache.set(flight.id, flight);
+		});
+
+		return response.data;
+	} catch (err: any) {
+		console.log(err?.response?.data);
+		return null;
+	}
+};
+
+export const minimizeFlightData = (flight: GoogleFlightData['best_flights'][number]) => {
+	// format ID: 1, departure
+
+	return {
+		id: flight.id,
+		departure_airport: flight.flights[0].departure_airport.id,
+		arrival_airport: flight.flights[flight.flights.length - 1].arrival_airport.id,
+		layoverDuration: flight.layovers.reduce((acc, layover) => acc + layover.duration, 0),
+	};
+};
+
+export const getCachedGoogleFlight = (id: string) => {
+	return departureFlightCache.get(id);
+};
+
+export const setCachedGoogleFlight = (id: string, data: any) => {
+	departureFlightCache.set(id, data);
+};
